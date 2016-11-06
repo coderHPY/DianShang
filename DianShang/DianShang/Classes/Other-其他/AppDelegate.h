681ed4ca6fc0ab420519b6b7252abbd6f67c66b7
何沛毅 <295223785@qq.com>
2016-11-06 09:19:31 +0800

    初始化

---
+++ DianShang/DianShang/Classes/Other-其他/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DianShang
+//
+//  Created by He on 16/11/5.
+//  Copyright © 2016年 He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
