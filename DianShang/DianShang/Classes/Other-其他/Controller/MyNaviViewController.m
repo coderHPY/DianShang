6f2c3f57768d74fc6a860f68dafa17f95a0e729f
何沛毅 <295223785@qq.com>
2016-11-06 17:12:40 +0800

    自定义navi

--- DianShang/DianShang/Classes/Other-其他/Controller/MyNaviViewController.m
+++ DianShang/DianShang/Classes/Other-其他/Controller/MyNaviViewController.m
@@ -8,30 +8,73 @@
 
 #import "MyNaviViewController.h"
 
-@interface MyNaviViewController ()
+@interface MyNaviViewController ()<UIGestureRecognizerDelegate>
 
 @end
 
 @implementation MyNaviViewController
 
++ (void)initialize
+{
+    // 设置UINavigationBar
+    UINavigationBar *bar = [UINavigationBar appearance];
+    [bar setBackgroundImage:[UIImage imageNamed:@""] forBarMetrics:UIBarMetricsDefault];
+    
+    NSMutableDictionary *barAttrs = [NSMutableDictionary dictionary];
+    barAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:20];
+    [bar setTitleTextAttributes:barAttrs];
+    
+    // 设置UIBarButtonItem
+    UIBarButtonItem *Item = [UIBarButtonItem appearance];
+    
+    NSMutableDictionary *normalItemAttrs = [NSMutableDictionary dictionary];
+    normalItemAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:17];
+    normalItemAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+    [Item setTitleTextAttributes:normalItemAttrs forState:UIControlStateNormal];
+    
+    NSMutableDictionary *disabledItemAttrs = [NSMutableDictionary dictionary];
+    disabledItemAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+    [Item setTitleTextAttributes:disabledItemAttrs forState:UIControlStateDisabled];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // 设置监听拖拽手势代理
+    self.interactivePopGestureRecognizer.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    if (self.childViewControllers.count >= 1) {
+        
+        // 创建button
+        UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [backButton setTitle:@"返回" forState:UIControlStateNormal];
+        [backButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [backButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+        [backButton setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+        [backButton setImage:[UIImage imageNamed:@""] forState:UIControlStateHighlighted];
+        [backButton sizeToFit];
+        [backButton addTarget:self action:@selector(backButtonClick) forControlEvents:UIControlEventTouchUpInside];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+        // push进来时隐藏底部bar
+        viewController.hidesBottomBarWhenPushed = YES;
+        backButton.contentEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0);
+    }
+    
+    [super pushViewController:viewController animated:animated];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)backButtonClick
+{
+    [self popViewControllerAnimated:YES];
 }
-*/
+
+// 每当用户触发【返回手势】时都会调用一次这个方法
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    return self.childViewControllers.count > 1;
+}
 
 @end
