0956427490d50d54ca12d28b03e79b73bd60cc8a
He <He@hedemacbook-pro.local>
2016-11-09 10:45:58 +0800

    在other中创建了person

---
+++ DianShang/DianShang/Classes/Other-其他/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  DianShang
+//
+//  Created by He on 16/11/9.
+//  Copyright © 2016年 He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ DianShang/DianShang/Classes/Other-其他/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  DianShang
+//
+//  Created by He on 16/11/9.
+//  Copyright © 2016年 He. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
